bdca924765d0a4a0ad408ba874b13156b8011cf8
Vineeth Puli <vpuli98@gmail.com>
2019-10-19 14:42:28 -0400

    Split

---
+++ Split/Split/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Split
+//
+//  Created by Vineeth Puli on 10/19/19.
+//  Copyright © 2019 HackSquad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
