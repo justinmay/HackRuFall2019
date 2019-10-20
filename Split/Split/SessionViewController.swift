ec6cfaafe6a0eefd5e3371a38deb15b86253df7d
Vineeth Puli <vpuli98@gmail.com>
2019-10-19 20:22:54 -0400

    some changes

--- Split/Split/SessionViewController.swift
+++ Split/Split/SessionViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class SessionViewController: UIViewController {
+class SessionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
-    var tableName : String!
-    
     @IBOutlet weak var partyLabel: UILabel!
+    @IBOutlet weak var partyTableView: UITableView!
+    
+    var partyPeople : PeopleInTable?
+    
+    var tableName : String!
+    var tableId : String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Justin's Kitchen - \(tableName!)"
+        self.partyTableView.delegate = self
+        self.partyTableView.dataSource = self
+        
+        DataManager.dataManager.getPeopleInTable(table: <#T##String#>)
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let peeps = partyPeople {
+                return peeps.people.count
+        }
+        return 0
 
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let peeps = partyPeople {
+            let tableViewCell = partyTableView.dequeueReusableCell(withIdentifier: "tables", for: indexPath)
+            tableViewCell.textLabel?.text = peeps.people[indexPath.row]
+            return tableViewCell
+        }
+        return UITableViewCell()
+    }
+    
     /*
     // MARK: - Navigation
 
